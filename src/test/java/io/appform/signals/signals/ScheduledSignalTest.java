763ba4a29861dc0c2b0453f997fcbe0551b9d882
Santanu Sinha <santanu.sinha@gmail.com>
2021-09-21 14:23:25 +0530

    Adjeusted delay in test

--- src/test/java/io/appform/signals/signals/ScheduledSignalTest.java
+++ src/test/java/io/appform/signals/signals/ScheduledSignalTest.java
@@ -117,8 +117,7 @@ class ScheduledSignalTest {
                 .timeout(15, TimeUnit.SECONDS)
                 .until(() -> ctr.get() > 5);
         val delay = new Date().getTime() - startTime.getTime();
-        System.out.println(delay);
-        assertTrue(delay > 8_500 && delay < 15_000); //Takes more time
+        assertTrue(delay > 6_000 && delay < 15_000); //Takes more time
         s.close();
     }
 
